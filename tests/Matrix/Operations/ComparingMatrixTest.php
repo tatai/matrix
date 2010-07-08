ee66048c506ea20f0a72eb07ad3ebc6d57782bc5
Tatai <fran.naranjo@gmail.com>
2010-07-08 23:17:22 +0200

    Comparing Matrix

---
+++ tests/Matrix/Operations/ComparingMatrixTest.php
@@ -0,0 +1,48 @@
+<?php
+class ComparingMatrixTest extends PHPUnit_Framework_TestCase {
+	private $_matrix = null;
+
+	public function setUp() {
+		$data = array(
+			array(1, 3, 3),
+			array(1, 4, 3),
+			array(1, 3, 4)
+		);
+		$this->_matrix = MatrixFactory::createFromArray(3, 3, $data);
+	}
+
+	/**
+	 * @test
+	 */
+	public function oneMatrixCompareWithItselfIsEqual() {
+		$this->assertTrue($this->_matrix->isEqual($this->_matrix));
+	}
+
+	/**
+	 * @test
+	 */
+	public function oneMatrixCompareWithAnotherWithSameDataIsEqual() {
+		$data = array(
+			array(1, 3, 3),
+			array(1, 4, 3),
+			array(1, 3, 4)
+		);
+		$matrix = MatrixFactory::createFromArray(3, 3, $data);
+		
+		$this->assertTrue($this->_matrix->isEqual($matrix));
+	}
+	
+	/**
+	 * @test
+	 */
+	public function oneSingleValueMakeMatrixDifferent() {
+		$data = array(
+			array(1, 3, 3),
+			array(1, 1, 3),
+			array(1, 3, 4)
+		);
+		$matrix = MatrixFactory::createFromArray(3, 3, $data);
+		
+		$this->assertFalse($this->_matrix->isEqual($matrix));
+	}
+}
